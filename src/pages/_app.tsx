1ced9aedee8a09eacb752d04121356cd5b272eba
Damla Koksal <me@damlakoksal.com>
2022-04-18 01:31:57 +0300

    refactor: change place of favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,7 @@ import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} 
-  
-  
-  
-  />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
